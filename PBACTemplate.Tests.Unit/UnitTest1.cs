7e8fbeb29a0239906a4762320fb5d23e66d838fd
brianlparker <brian.parker@hotmail.com.au>
2026-01-04 20:15:09 +1100

    Add unit test project with initial test setup
    Added PBACTemplate.Tests.Unit to the solution, targeting .NET 10.0 and referencing the main project. Included xUnit, Moq, FluentAssertions, and coverlet for testing and coverage. Created a placeholder UnitTest1.cs with a basic test class and empty test method.

---
+++ PBACTemplate.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2026, Brian Parker. All Rights Reserved.
+// UnitTest1.cs See LICENSE.txt in the root folder of the solution.
+
+namespace PBACTemplate.Tests.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
