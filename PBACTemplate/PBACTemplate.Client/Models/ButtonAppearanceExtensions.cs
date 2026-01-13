5cff4deb1f065d23cd5d6538bfa40a61e45a3c02
brianlparker <brian.parker@hotmail.com.au>
2026-01-13 13:18:31 +1100

    Add ButtonAppearance enum and support for button styling
    Introduce ButtonAppearance enum and extension for mapping to Fluent UI styles. Update AppButton and ButtonBase to accept Appearance parameter. Refactor usage in Counter.razor and Home.razor to demonstrate primary button styling. Update _Imports.razor for model access. Enables consistent, extensible button appearance control.

---
+++ PBACTemplate/PBACTemplate.Client/Models/ButtonAppearanceExtensions.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2026, Brian Parker. All Rights Reserved.
+// ButtonAppearanceExtensions.cs See LICENSE.txt in the root folder of the solution.
+
+namespace PBACTemplate.Client.Models;
+
+public static class ButtonAppearanceExtensions
+{
+    extension(ButtonAppearance appearance)
+    {
+        public Microsoft.FluentUI.AspNetCore.Components.Appearance FluentAppearance
+        {
+            get
+            {
+                return appearance switch
+                {
+                    ButtonAppearance.Default => Microsoft.FluentUI.AspNetCore.Components.Appearance.Neutral,
+                    ButtonAppearance.Primary => Microsoft.FluentUI.AspNetCore.Components.Appearance.Accent,
+                    _ => Microsoft.FluentUI.AspNetCore.Components.Appearance.Neutral,
+                };
+            }
+        }
+    }
+}
