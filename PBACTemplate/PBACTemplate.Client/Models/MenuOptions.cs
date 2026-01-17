92a1665ae481d1bc6ca9fdb19810fc5061a29f14
brianlparker <brian.parker@hotmail.com.au>
2026-01-17 15:59:49 +1100

    Add Fluent UI left-side panel menu with dialog support
    Refactored navigation to use a Fluent UI side panel opened via dialog, replacing the old sidebar. Added MenuOptions model for menu configuration. Updated layout and provider files to support new navigation and render mode. Conditional menu items now reflect user authorization state.

---
+++ PBACTemplate/PBACTemplate.Client/Models/MenuOptions.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2026, Brian Parker. All Rights Reserved.
+// MenuOptions.cs See LICENSE.txt in the root folder of the solution.
+
+namespace PBACTemplate.Client.Models;
+
+public class MenuOptions
+{
+    public string? AppTitle { get; set; }
+}
